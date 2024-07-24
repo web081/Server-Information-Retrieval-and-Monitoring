f8d5c72cf4aa3def19ec3d7a4a604d88c4531169
Kole <koladevops@outlook.com>
2024-07-24 11:01:16 +0100

    update  action

--- tests/test_devopsfetch.py
+++ tests/test_devopsfetch.py
@@ -1,17 +1,12 @@
-# tests/test_devopsfetch.py
-
 import unittest
-from devopsfetch import fetch_ports, fetch_users
+from devopsfetch import list_users
 
 class TestDevOpsFetch(unittest.TestCase):
 
-    def test_ports(self):
-        result = fetch_ports()
-        self.assertIsInstance(result, list)
-
-    def test_users(self):
-        result = fetch_users()
-        self.assertIsInstance(result, list)
+    def test_list_users(self):
+        result = list_users()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
 
 if __name__ == '__main__':
     unittest.main()
